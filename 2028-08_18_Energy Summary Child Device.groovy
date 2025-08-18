fbf9dfbf0e427415a339fb51f11ed870bba984bd
Im4Fun <46326473+Im4Fun@users.noreply.github.com>
2025-08-18 22:09:24 +0200

    Update 2028-08_18_Energy Summary Child Device.groovy

--- 2028-08_18_Energy Summary Child Device.groovy
+++ 2028-08_18_Energy Summary Child Device.groovy
@@ -1,24 +1,25 @@
 /**
- *  Energy Summary Child Device
+ *  Energy Summary Device
  *
  *  Child device used by the Estimated Energy (Bulbs & Switches) app
- *  and Energy Cost agrregator app to expose totals and current values.
+ *  and Energy Cost agregator app to expose totals and current values.
  *
  *  Author: Carl Rådetorp
- *  Version: 1.3.0
+ *  Version: 1.5.0
  *  Date: 2025-08-18
  *
  *  Capabilities:
- *    - PowerMeter  (attribute: power, W)   → current total instantaneous watts
- *    - EnergyMeter (attribute: energy, kWh) → running monthly energy
+ *    - PowerMeter  (attribute: power, W)
+ *    - EnergyMeter (attribute: energy, kWh)
  *
- *  Custom attributes (NUMBER unless noted):
+ *  Custom attributes:
  *    - todayEnergy   (kWh, reset daily)
  *    - monthEnergy   (kWh, reset monthly)
- *    - price         (current price per kWh incl. extra fees)
+ *    - price         (current price per kWh incl. fees)
  *    - todayCost     (currency, today’s cost)
  *    - monthCost     (currency, this month’s cost)
  *    - lastUpdated   (STRING, timestamp)
+ *    - summaryHtml   (STRING, pre-formatted summary for dashboards)
  */
 
 metadata {
@@ -38,6 +39,7 @@ metadata {
         attribute "todayCost", "NUMBER"
         attribute "monthCost", "NUMBER"
         attribute "lastUpdated", "STRING"
+        attribute "summaryHtml", "STRING"
     }
 }
 
@@ -49,10 +51,45 @@ def updated() {
     log.info "${device.displayName} updated"
 }
 
-// Hubitat dashboard / RM can call Refresh
+// Dashboard / RM can call Refresh → ask parent app to push new values
 def refresh() {
     log.debug "Refresh called on ${device.displayName}"
     if (parent) {
-        parent.updateChild([refresh:true])
+        parent.updateChild()
     }
+}
+
+/**
+ * Catch events sent from the parent app and rebuild summaryHtml
+ */
+def parse(String description) {
+    // Not used, but required by Hubitat
+}
+
+def sendEvent(Map evt) {
+    // Intercept events from parent
+    super.sendEvent(evt)
+    if (["todayEnergy","monthEnergy","price","todayCost","monthCost","power","lastUpdated"].contains(evt.name)) {
+        runInMillis(100, "rebuildSummary")
+    }
+}
+
+def rebuildSummary() {
+    def todayEnergy = device.currentValue("todayEnergy") ?: "-"
+    def todayCost   = device.currentValue("todayCost")   ?: "-"
+    def monthEnergy = device.currentValue("monthEnergy") ?: "-"
+    def monthCost   = device.currentValue("monthCost")   ?: "-"
+    def price       = device.currentValue("price")       ?: "-"
+    def power       = device.currentValue("power")       ?: "-"
+    def lastUpdated = device.currentValue("lastUpdated") ?: "-"
+
+    def summary = """<div style='font-family:sans-serif;'>
+<b>Today:</b> ${todayEnergy} kWh / ${todayCost}<br>
+<b>Month:</b> ${monthEnergy} kWh / ${monthCost}<br>
+<b>Now:</b> ${power} W<br>
+<b>Price:</b> ${price} /kWh<br>
+<i>Updated:</i> ${lastUpdated}
+</div>"""
+
+    super.sendEvent(name: "summaryHtml", value: summary)
 }
